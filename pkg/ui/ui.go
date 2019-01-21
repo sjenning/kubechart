97a9bf64cc96dd29c025cd9928adf70a67de3e30
Seth Jennings <sjenning@redhat.com>
2019-01-21 17:23:14 -0600

    add pod log access

--- pkg/ui/ui.go
+++ pkg/ui/ui.go
@@ -1,15 +1,34 @@
 package ui
 
 import (
+	"io"
 	"net/http"
 
 	"github.com/golang/glog"
+	"github.com/gorilla/mux"
 	"github.com/sjenning/kubechart/pkg/event"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
 )
 
-func Run(store event.Store) {
-	http.Handle("/", http.FileServer(http.Dir("./static")))
-	http.HandleFunc("/data.json", store.JSONHandler)
+func Run(store event.Store, client kubernetes.Interface) {
+	r := mux.NewRouter()
+	r.Handle("/", http.FileServer(http.Dir("./static")))
+	r.HandleFunc("/data.json", store.JSONHandler)
+	r.HandleFunc("/logs/{namespace}/{podname}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		namespace, podname := vars["namespace"], vars["podname"]
+		req := client.CoreV1().Pods(namespace).GetLogs(podname, &v1.PodLogOptions{})
+		podLogs, err := req.Stream()
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}
+		defer podLogs.Close()
+		_, err = io.Copy(w, podLogs)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}
+	})
 	glog.Infof("Listening on :3000")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", r)
 }
